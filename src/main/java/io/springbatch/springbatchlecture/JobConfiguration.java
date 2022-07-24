8b9f395305cb96069e513b388b6c7776a77841de
megasoby <megasoby@ssg.com>
2022-07-24 15:41:53 +0900

    batch inint

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobParameterConfiguration {
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -52,5 +52,4 @@ public class JobParameterConfiguration {
                 })
                 .build();
     }
-
 }
